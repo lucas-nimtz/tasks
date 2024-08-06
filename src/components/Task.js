a77e3ce3c5d2da31f6489b1155920ed998b1682b
lucas-nimtz <lucassanchesnimtz@gmail.com>
2024-08-06 07:55:08 -0300

    novas coisas

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,30 @@
-import React, {Component} from "react"
-import {StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native"
-import { Icon } from "react-native-vector-icons/FontAwesome6"
+import React from "react"
+import { Text, View, StyleSheet, TouchableWithoutFeedback } from "react-native"
 import moment from "moment"
-import 'moment/locale/pt-br'
+import Icon from "react-native-vector-icons/FontAwesome6"
 
 export default props => {
-    const date = props.done_at != null ? props.done_at : props.estimate_at
-    const formated_date = moment(date).format('ddd, D [de] MMM')
+    const done_or_not = props.done_at != null ? {textDecorationLine: "line-through"}:{} 
+    const date = props.done_at != null ? props.done_at : date = props.estimate_at
+    
+    /* if(props.done_at != null){
+        date = props.done_at
+    }
+    else{
+        date = props.estimate_at
+    } */
+
     return(
         <View style={styles.container}>
-            <TouchableWithoutFeedback onPress={() => alert("Olá")}>
+            <TouchableWithoutFeedback>
                 <View style={styles.checkContainer}>
                     {get_Check_View(props.done_at)}
                 </View>
             </TouchableWithoutFeedback>
             <View>
-                <Text style={styles.desc}>{props.description}</Text>
-                <Text style={styles.desc}>{formated_date}</Text>
+                <Text style={[styles.desc, done_or_not]}>{props.description}</Text>
+                <Text style={styles.date}>{props.estimate_at + ""}</Text>
+                <Text style={styles.date}>{props.done_at + ""}</Text>
             </View>
         </View>
     )
@@ -39,39 +47,41 @@ function get_Check_View(done_at){
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection: "row",
+    container: {
+        flexDirection: 'row',
         borderColor: '#AAA',
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10
     },
-    desc:{
-        fontFamly: 'Arial',
-        fontSize: 20
-    },
-    date:{
-        fontFamly: 'Arial'
-    },
-    checkContainer:{
+    checkContainer: {
         width: "20%",
-        alighItems: "center",
-        justifyContent: "center"
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    done:{
+    done: {
         height: 25,
         width: 25,
         borderRadius: 13,
         borderWidth: 1,
         backgroundColor: "#4D7031",
-        alignItems: "center",
-        justifyContent: "center" 
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    pending:{
+    pending: {
         height: 25,
         width: 25,
         borderRadius: 13,
         borderWidth: 1,
-        borderColor: "#555555"
+        borderColor: "#555"
+    },
+    desc: {
+        fontFamily: "Arial",
+        fontSize: 20,
+        color: "#333"
+    },
+    date: {
+        fontFamily: "Arial",
+        color: "#555"
     }
 })
